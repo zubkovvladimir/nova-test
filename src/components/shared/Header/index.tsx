4e467739932ff87f0c7731ede4f3c6f397842def
zubkovvladimir <njoy70@mail.ru>
2022-09-19 23:19:59 +0500

    markup | feat: создал компонент кнопки

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 
+import HeaderLogo from 'assets/images/logo.png';
 import { useTitle } from 'ahooks';
 import { appName } from 'constants/app';
 
 import classes from './MainPage.module.scss';
+import { Button } from '../Button';
 
 const Header: FC = () => (
   <header>
-    <p>Header</p>
+    <img alt="Ведьмак" src={HeaderLogo} />
+    <Button>Подключить подписку</Button>
   </header>
 );
 
