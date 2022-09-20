ef5cd2c48a776d1e2201b114fa6838e1dbdbcbc6
zubkovvladimir <njoy70@mail.ru>
2022-09-20 07:22:56 +0500

    styles | feat: стилизовал шапку

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,20 +1,17 @@
 import { FC } from 'react';
 
-import HeaderLogo from 'assets/images/logo.png';
-
 import { Button } from '../Button';
 import { Container } from '../Container';
+import Logo from '../Logo';
 import classes from './Header.module.scss';
 
 const Header: FC = () => (
   <header className={classes.header}>
     <Container>
       <div className={classes.header__inner}>
-        <a href="/">
-          <img alt="Ведьмак" src={HeaderLogo} />
-        </a>
+        <Logo />
 
-        <Button>Подключить подписку</Button>
+        <Button variant="outlined">Подключить подписку</Button>
       </div>
     </Container>
   </header>
