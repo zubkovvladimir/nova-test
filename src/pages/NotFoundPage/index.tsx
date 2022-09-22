bf4258eed8758901fb5efa38956e1691056d5ae0
zubkovvladimir <njoy70@mail.ru>
2022-09-22 10:09:14 +0500

    добавил деплой

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import { Routes } from 'constants/routes';
-
-const NotFoundPage: React.FC = () => {
-  const navigate = useNavigate();
-
-  return <div>Запрашиваемая страница не найдена</div>;
-};
+const NotFoundPage: React.FC = () => <div>Запрашиваемая страница не найдена</div>;
 
 export default NotFoundPage;
