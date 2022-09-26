bb7e5750f6903da6c7a7dbf1f58466367f03f23a
zubkovvladimir <njoy70@mail.ru>
2022-09-26 17:02:55 +0500

    fix: исправил относительные пути

--- webpack/parts/entry-output.js
+++ webpack/parts/entry-output.js
@@ -13,7 +13,7 @@ function getEntryOutput() {
     output: {
       filename: isProd ? '[name].js' : '[name].[contenthash].js',
       path: path.resolve(__dirname, '../../build'),
-      publicPath: '/novatest/',
+      publicPath: '/',
     },
   };
 }
